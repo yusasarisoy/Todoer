7d8b2d53776a84b5246003fba042816509401919
Yuşa Sarısoy <yusasarisoy@icloud.com>
2021-06-19 16:26:56 +0300

    ☎️ Manage checkboxes using callbacks.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'Hello',
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(
+          color: Colors.white,
+          decoration:
+              isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+        ),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) => {},
-      ),
+      trailing: TaskCheckbox(isChecked, (bool? checkboxState) {
+        if (checkboxState != null) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        }
+      }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final ValueChanged<bool?> toggleCheckboxState;
+
+  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.pinkAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
